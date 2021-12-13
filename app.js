e3b33313b181bdd566fece30ed522785990b6269
Carl-RobertMots <72864477+Carl-RobertMots@users.noreply.github.com>
2021-12-13 14:41:40 +0200

    Ettevalmistus ja api key lisamine

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded, cityWeather")
+
+function weatherDataFetch(city) {
+    var key = "5d04eaa8e329daf98c6da9c9c87a2b7f";
+    fetch(`https://api.openweather.map.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then(function(resp) {
+        return resp.json()
+    })
+        .then(function (data){
+            console.log(data);
+        })
+        .catch(function() {
+
+        })
+}
